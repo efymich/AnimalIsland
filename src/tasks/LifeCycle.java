c7fb6894d09be843cd95ac45b6d48fbd5b54cfd9
efymich <nn.efymovich@gmail.com>
2023-11-15 20:28:42 +0200

    remove Semaphores

--- src/tasks/LifeCycle.java
+++ src/tasks/LifeCycle.java
@@ -17,10 +17,6 @@ public class LifeCycle implements Runnable {
     private final EatProcess eatProcess = EatProcess.getInstance();
     private final MoveProcess moveProcess = MoveProcess.getInstance();
 
-    private final Semaphore eatingSemaphore = new Semaphore(3);
-    private final Semaphore reproducingSemaphore = new Semaphore(0);
-    private final Semaphore movingSemaphore = new Semaphore(0);
-
     public LifeCycle(Island island) {
         this.island = island;
     }
@@ -35,102 +31,74 @@ public class LifeCycle implements Runnable {
 //                TimeUnit.SECONDS.sleep(3);
             } /*catch (InterruptedException e) {
                 throw new RuntimeException(e);
-            } */
-            finally {
+            } */ finally {
 
             }
         }
     }
 
     private void eating(Island island) {
-        try {
-            eatingSemaphore.acquire();
 //            System.out.println(Thread.currentThread().getName());
-            Utility.getIslandCellStream(island)
+        Utility.getIslandCellStream(island)
 //                    .peek(System.out::println)
-                    .forEach(list -> {
-                        AtomicInteger outerAtom = new AtomicInteger(0);
-                        AtomicInteger innerAtom = new AtomicInteger(1);
-                        for (int i = 0; i < list.size(); i++) {
-                            if (outerAtom.compareAndSet(i,i++)) {
-                                double saturationLimit = list.get(i).getSaturationLimit();
-                                double saturation = 0.0;
+                .forEach(list -> {
+                    AtomicInteger outerAtom = new AtomicInteger(0);
+                    AtomicInteger innerAtom = new AtomicInteger(1);
+                    for (int i = 0; i < list.size(); i++) {
+                        if (outerAtom.compareAndSet(i, i++)) {
+                            double saturationLimit = list.get(i).getSaturationLimit();
+                            double saturation = 0.0;
 
-                                for (int j = i + 1; j < list.size(); j++) {
-                                    if (innerAtom.compareAndSet(j,j++)) {
-                                        if (saturation > saturationLimit) break;
-                                        if (eatProcess.isEatable(list.get(i), list.get(j))) {
-                                            saturation += list.get(j).getWeight();
-                                            list.remove(j);
-                                        }
+                            for (int j = i + 1; j < list.size(); j++) {
+                                if (innerAtom.compareAndSet(j, j++)) {
+                                    if (saturation > saturationLimit) break;
+                                    if (eatProcess.isEatable(list.get(i), list.get(j))) {
+                                        saturation += list.get(j).getWeight();
+                                        list.remove(j);
                                     }
                                 }
                             }
                         }
-                    });
+                    }
+                });
 //            System.out.println("----------EATING-------------------------------");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            eatingSemaphore.release();
-//            eatingSemaphore.drainPermits();
-            reproducingSemaphore.release();
-        }
     }
 
     private void reproducing(Island island) {
-        try {
-            reproducingSemaphore.acquire();
 
 //            System.out.println(Thread.currentThread().getName());
-            Utility.getIslandCellStream(island)
+        Utility.getIslandCellStream(island)
 //                    .peek(System.out::println)
-                    .forEach(list -> {
-                        Map<Entities, Long> mapGroupByCount = list.stream().collect(Collectors.groupingBy(Entity::getKind, Collectors.counting()));
-                        mapGroupByCount.keySet().forEach(key -> {
-                            long countEntitiesOnCell = mapGroupByCount.get(key);
-                            int countOfNewBorn = (int) Math.floorDiv(countEntitiesOnCell, 2);
-                            if (key != Entities.PLANT && key.getCountOnCell() > (countEntitiesOnCell + countOfNewBorn)) {
-                                for (int i = 0; i < countOfNewBorn; i++) {
-                                    Utility.createEntityAndAdd(list, key);
-                                }
+                .forEach(list -> {
+                    Map<Entities, Long> mapGroupByCount = list.stream().collect(Collectors.groupingBy(Entity::getKind, Collectors.counting()));
+                    mapGroupByCount.keySet().forEach(key -> {
+                        long countEntitiesOnCell = mapGroupByCount.get(key);
+                        int countOfNewBorn = (int) Math.floorDiv(countEntitiesOnCell, 2);
+                        if (key != Entities.PLANT && key.getCountOnCell() > (countEntitiesOnCell + countOfNewBorn)) {
+                            for (int i = 0; i < countOfNewBorn; i++) {
+                                Utility.createEntityAndAdd(list, key);
                             }
-                        });
+                        }
                     });
+                });
 //            System.out.println("-------------REPRODUCING----------------------------");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            reproducingSemaphore.release();
-//            reproducingSemaphore.drainPermits();
-            movingSemaphore.release();
-        }
     }
 
     private void moving(Island island) {
-        try {
-            movingSemaphore.acquire();
 
 //            System.out.println(Thread.currentThread().getName());
-            Map<Integer, Map<Integer, CopyOnWriteArrayList<Entity>>> islandMap = island.getIslandMap();
+        Map<Integer, Map<Integer, CopyOnWriteArrayList<Entity>>> islandMap = island.getIslandMap();
 
-            for (Integer xKey : islandMap.keySet()) {
-                Map<Integer, CopyOnWriteArrayList<Entity>> innerMap = islandMap.get(xKey);
-                for (Integer yKey : innerMap.keySet()) {
-                    CopyOnWriteArrayList<Entity> cell = innerMap.get(yKey);
+        for (Integer xKey : islandMap.keySet()) {
+            Map<Integer, CopyOnWriteArrayList<Entity>> innerMap = islandMap.get(xKey);
+            for (Integer yKey : innerMap.keySet()) {
+                CopyOnWriteArrayList<Entity> cell = innerMap.get(yKey);
 //                    System.out.println(cell);
-                    cell.forEach(entity -> {
-                        moveProcess.move(island, xKey, yKey, entity);
-                    });
-                }
+                cell.forEach(entity -> {
+                    moveProcess.move(island, xKey, yKey, entity);
+                });
             }
-//            System.out.println("--------------MOVING--------------------------------");
-        } catch (InterruptedException e) {
-            throw new RuntimeException(e);
-        } finally {
-            movingSemaphore.release();
-//            movingSemaphore.drainPermits();
-//            eatingSemaphore.release();
         }
+//            System.out.println("--------------MOVING--------------------------------");
     }
 }
